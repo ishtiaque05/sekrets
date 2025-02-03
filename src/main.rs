3b7004e45128ba4b3e3a6f43c1bb3facb2ac8764
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2025-02-03 00:37:15 -0800

    fix cmd

--- src/main.rs
+++ src/main.rs
@@ -9,63 +9,83 @@ mod types;
 mod parser;
 
 fn main() -> Result<()> {
-    let matches = Command::new("My CLI App")
-        .version("1.0")
-        .author("Syed Ishtiaque Ahmad")
-        .about("Sssshhh its a secret!!")
-        .arg(
-            Arg::new("encrypt")
-                .long("encrypt")
-                .value_name("FILE")
-                .help("Encrypts the specified file")
-                .action(ArgAction::Set),
-        )
-        .arg(
-            Arg::new("sekrets")
-                .long("sekrets")
-                .value_name("FILE")
-                .help("Decrypts the encrypted file to reveal passwords")
-                .action(ArgAction::Set),
-                
-        )
-        .arg(
-            Arg::new("keyword")
-                .help("The keyword for which to retrieve the password, e.g., --github")
-                .long_help("Specific keyword like --github to fetch the password for GitHub")
-                .action(ArgAction::Append),
-        )
-        .get_matches();
+    let matches = Command::new("sekret")
+    .version("1.0")
+    .author("Syed Ishtiaque Ahmad")
+    .about("Sssshhh it's a secret!!")
+    .subcommand_required(true)
+    .arg_required_else_help(true)
+    
+    .subcommand(
+        Command::new("encrypt")
+            .about("Encrypt a file")
+            .arg(
+                Arg::new("file")
+                    .short('f')
+                    .long("file")
+                    .value_name("FILE")
+                    .help("File to encrypt")
+                    .required(true)
+                    .action(ArgAction::Set),
+            ),
+    )
+    .subcommand(
+        Command::new("decrypt")
+            .about("Decrypt a file and retrieve account credentials")
+            .arg(
+                Arg::new("file")
+                    .short('f')
+                    .long("file")
+                    .value_name("FILE")
+                    .help("File to decrypt")
+                    .required(true)
+                    .action(ArgAction::Set),
+            )
+            .arg(
+                Arg::new("accounts")
+                    .short('a')
+                    .long("accounts")
+                    .value_name("ACCOUNT")
+                    .help("Account(s) for which to retrieve credentials (e.g., github bank)")
+                    .required(true)
+                    .action(ArgAction::Append),
+            ),
+    )
+    .get_matches();
 
-    if let Some(file) = matches.get_one::<String>("encrypt") {
-        println!("File to encrypt: {}", file);
+    match matches.subcommand() {
+        Some(("encrypt", sub_matches)) => {
+            let file = sub_matches
+                .get_one::<String>("file")
+                .expect("File is required");
+            println!("Encrypting file: {}", file);
+            println!("Enter password: ");
+            let password = read_password().expect("Failed to read password");
+            let encrypted_file = encryptor::encrypt_file(file, password.as_str())?;
+            println!("Encrypted file created: {}", encrypted_file);
+        }
+        Some(("decrypt", sub_matches)) => {
+            let file = sub_matches
+                .get_one::<String>("file")
+                .expect("File is required");
+            let accounts: Vec<&String> = sub_matches
+                .get_many::<String>("accounts")
+                .expect("At least one account is required")
+                .collect();
 
-        println!("Enter password: ");
-        let password = read_password().expect("Failed to read password");
-        
-        let res = encryptor::encrypt_file(file, password.as_ref()).unwrap();
-
-        println!("encrypted file is {:?}", res.clone());
-        println!(
-            "Decrypt file {:?}",
-            decryptor::decrypt_file(res.as_str(), password.as_ref())
-        );
-    }
-
-    if let Some(crendential_file) = matches.get_one::<String>("sekrets") {
-        println!("Credentials file path: {}", crendential_file);
-
-        if let Some(keywords) = matches.get_many::<String>("keyword") {
-            for keyword in keywords {
-                println!("Keyword is {}", keyword);
-
-                println!("Enter password: ");
-                let password = read_password().expect("Failed to read password");
-                let data = decryptor::decrypt_file(crendential_file.as_str(), password.as_ref())?;
-
-                let result = Credential::new(keyword.to_string()).get_credentials(data)?;
-                println!("Account: {} - Username: {}, Password: {}", keyword, result.username, result.password);
+            println!("Decrypting file: {}", file);
+            println!("Enter password: ");
+            let password = read_password().expect("Failed to read password");
+            let decrypted_data = decryptor::decrypt_file(file, password.as_str())?;
+            for account in accounts {
+                let result = Credential::new(account.to_string()).get_credentials(decrypted_data.clone())?;
+                println!(
+                    "Account: {} - Username: {}, Password: {}",
+                    account, result.username, result.password
+                );
             }
         }
+        _ => unreachable!(),
     }
 
     Ok(())
