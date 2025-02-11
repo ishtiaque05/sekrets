7798ffc9deabe8dd86c45b64c3a5790be3e97100
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2025-02-10 17:12:58 -0800

    clean up

--- src/encryptor.rs
+++ src/encryptor.rs
@@ -13,61 +13,77 @@ use std::io::{Read, Write};
 
 pub const ENCRYPTED_FILENAME: &str = "sekrets.enc";
 
-pub fn encrypt_file(filename: &str, password: &str) -> Result<String, FileError> {
+fn derive_encryption_key(password: &str) -> Result<(Aes256Gcm, SaltString, [u8; 12]), FileError> {
     let salt = SaltString::generate(&mut OsRng);
-
     let argon2 = Argon2::default();
+    
     let password_hash = argon2
         .hash_password(password.as_bytes(), &salt)
         .map_err(|err| FileError::HashingError(err.to_string()))?;
-
     
     let hash_output = password_hash
         .hash
         .ok_or_else(|| FileError::InvalidHashOutput("Hash output is empty".to_string()))?;
 
-   
     let key_bytes = &hash_output.as_bytes()[..32];
-    let key = Aes256Gcm::new_from_slice(key_bytes).expect("Failed to create key from hash");
+    let key = Aes256Gcm::new_from_slice(key_bytes)
+        .map_err(|_| FileError::KeyGenerationError("Failed to create key from hash".to_string()))?;
 
-    let nonce_bytes: [u8; 12] = key_bytes[..12]
+    let nonce: [u8; 12] = key_bytes[..12]
         .try_into()
         .map_err(|_| FileError::InvalidNonceSize("Invalid nonce size".to_string()))?;
-    let nonce = Nonce::from_slice(&nonce_bytes);
 
-   
+    Ok((key, salt, nonce))
+}
+
+fn read_file_contents(filename: &str) -> Result<Vec<u8>, FileError> {
     let mut file = File::open(filename).map_err(|err| FileError::FileReadError(err.to_string()))?;
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)
         .map_err(|err| FileError::FileReadError(err.to_string()))?;
-
-    
-    let tag = key
-        .encrypt_in_place_detached(nonce, b"", &mut buffer)
-        .map_err(|err| FileError::EncryptionError(err.to_string()))?;
-
-    buffer.extend_from_slice(tag.as_slice());
-
-    
-    let encrypted_filename = get_encrypted_file_path(ENCRYPTED_FILENAME).to_string_lossy().into_owned();
-
-    let mut encrypted_file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(&encrypted_filename)
-        .map_err(|err| FileError::FileWriteError(err.to_string()))?;
-
-   
-    writeln!(encrypted_file, "{}", salt.as_str())
-        .map_err(|err| FileError::FileWriteError(err.to_string()))?;
-
-   
-    encrypted_file
-        .write_all(&buffer)
-        .map_err(|err| FileError::FileWriteError(err.to_string()))?;
-
-    Ok(encrypted_filename)
+    Ok(buffer)
 }
 
+fn encrypt_data(key: &Aes256Gcm, nonce: &[u8; 12], data: &mut Vec<u8>) -> Result<Vec<u8>, FileError> {
+    let nonce = Nonce::from_slice(nonce);
+    
+    let tag = key
+        .encrypt_in_place_detached(nonce, b"", data)
+        .map_err(|err| FileError::EncryptionError(err.to_string()))?;
+
+    data.extend_from_slice(tag.as_slice());
+    Ok(data.clone())
+}
+
+fn write_encrypted_file(filepath: &str, salt: &SaltString, data: &[u8]) -> Result<(), FileError> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(filepath)
+        .map_err(|err| FileError::FileWriteError(err.to_string()))?;
+
+    writeln!(file, "{}", salt.as_str())
+        .map_err(|err| FileError::FileWriteError(err.to_string()))?;
+
+    file.write_all(data)
+        .map_err(|err| FileError::FileWriteError(err.to_string()))?;
+
+    Ok(())
+}
+
+pub fn encrypt_file(filename: &str, password: &str) -> Result<String, FileError> {
+    let (key, salt, nonce) = derive_encryption_key(password)?;
+
+    let mut file_contents = read_file_contents(filename)?;
+    let encrypted_data = encrypt_data(&key, &nonce, &mut file_contents)?;
+
+    let encrypted_filepath = get_encrypted_file_path(ENCRYPTED_FILENAME)
+        .to_string_lossy()
+        .into_owned();
+
+    write_encrypted_file(&encrypted_filepath, &salt, &encrypted_data)?;
+
+    Ok(encrypted_filepath)
+}
 #[cfg(test)]
 mod tests;
