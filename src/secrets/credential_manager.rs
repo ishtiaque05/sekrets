09321f352f41b6c5e403687a856cd7c602729fd3
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2025-06-02 20:22:15 -0700

    sekrets decrypt -a  without username matches all account that contains (#22)
    * decrypt -a  without username matches all account that contains

--- src/secrets/credential_manager.rs
+++ src/secrets/credential_manager.rs
@@ -52,7 +52,7 @@ impl CredentialManager {
             .credentials
             .iter()
             .filter_map(|((acct, _), credential)| {
-                if acct == &account {
+                if acct.to_lowercase().contains(&account.to_lowercase()) {
                     Some(credential.clone())
                 } else {
                     None
